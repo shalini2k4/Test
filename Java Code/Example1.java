7880eb74e886b4ad1f242ad208c0b9b8a277c490
Sweety Shalini <sweety.shalini@altran.com>
2022-02-09 15:04:34 +0530

    Difftool

--- Java Code/Example1.java
+++ Java Code/Example1.java
@@ -2,5 +2,6 @@ public class Example1{
     public static void main(String[] args){
         System.out.println("To learn Git");
         System.out.println("Editing file and then pushing code");
+        System.out.println("difftool git command");
     }
 }
